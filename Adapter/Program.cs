30ffb972e6416ac6c9a44280c0beaf833c008b36
Rodrigo Filomeno <Rodrigo Filomeno>
2017-05-10 10:03:04 -0300

    Adapter

---
+++ Adapter/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Adapter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cliente cliente = new Cliente()
+            {
+                Nome = "Rodrigo",
+                Endereco = "Av. Rio Branco, 156",
+                DataNascimento = DateTime.Now.ToString("dd/MM/yyyy")
+            };
+
+            string xml = new GeradorDeXml().GeraXml(cliente);
+
+            Console.WriteLine(xml);
+        }
+    }
+}
